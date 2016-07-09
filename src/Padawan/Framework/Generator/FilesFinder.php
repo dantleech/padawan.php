1a11db36a35371fe790958d3e71af74d60c74713
dantleech <dan.t.leech@gmail.com>
2016-07-09 11:24:19 +0100

    Use Symfony finder

--- src/Padawan/Framework/Generator/FilesFinder.php
+++ src/Padawan/Framework/Generator/FilesFinder.php
@@ -8,6 +8,9 @@ use Padawan\Domain\Generator\FilesFinder as FilesFinderInterface;
 
 class FilesFinder implements FilesFinderInterface
 {
+    /** @var PathResolver */
+    private $path;
+
     public function __construct(PathResolver $path)
     {
         $this->path = $path;
@@ -15,30 +18,23 @@ class FilesFinder implements FilesFinderInterface
 
     public function findProjectFiles(Project $project)
     {
-        return $this->filterFiles(
-            $project,
-            $this->path->getDirFilesRecursive(
-                $project->getRootDir()
-            )
-        );
+        $finder = new Finder();
+        $finder->name('*.php');
+        $finder->ignoreVCS(true);
+        $finder->notPath('/.*\/.*vendor/');
+        $finder->in($project->getRootDir());
+
+        $files = [];
+
+        foreach ($finder as $fileInfo) {
+            $files[] = $this->path->relative($project->getRootDir(), $fileInfo->getPathname());
+        }
+
+        return $files; 
     }
 
     public function findChangedProjectFiles(Project $project)
     {
         throw new \Exception("Not implemented yet");
     }
-
-    protected function filterFiles(Project $project, $files)
-    {
-        $projectFiles = [];
-        foreach ($files as $file) {
-            if (!preg_match('/\.php$/', $file)) {
-                continue;
-            }
-            $projectFiles[] = $this->path->relative($project->getRootDir(), $file);
-        }
-        return $projectFiles;
-    }
-    /** @var PathResolver */
-    private $path;
 }
